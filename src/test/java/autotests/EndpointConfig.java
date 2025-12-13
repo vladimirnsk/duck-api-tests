355ec312a232bfe69ff5d3d1660dcaf9c746eda6
Vladimir Repin <heeqed@mail.ru>
2025-12-14 01:19:15 +0700

    Added SingleConnectionDataSource

--- src/test/java/autotests/EndpointConfig.java
+++ src/test/java/autotests/EndpointConfig.java
@@ -3,13 +3,25 @@ package autotests;
 import com.consol.citrus.http.client.HttpClient;
 import com.consol.citrus.http.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 public class EndpointConfig {
 
     @Bean
-    public HttpClient duckService(){
+    public HttpClient duckService() {
         return new HttpClientBuilder()
                 .requestUrl("http://localhost:2222")
                 .build();
-        }
     }
+
+    @Bean("testDb")
+    public SingleConnectionDataSource db() {
+        SingleConnectionDataSource dataSource = new SingleConnectionDataSource();
+
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:tcp://localhost:9092/mem:ducks");
+        dataSource.setUsername("dev");
+        dataSource.setPassword("dev");
+        return dataSource;
+    }
+}
